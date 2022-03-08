abcca240c3635a9a5cbda04eff5cbf97838578ac
HereW3G0Again <100608469+HereW3G0Again@users.noreply.github.com>
2022-03-08 23:20:42 +0300

    Add files via upload

---
+++ Employee.h
@@ -0,0 +1,15 @@
+#ifndef EMPLOYEE_H
+#define EMPLOYEE_H
+#include <string>
+using namespace std;
+
+struct Employee
+{
+    string surname;
+    string name;
+    string patronymic;
+    string qualificationLevel;
+    string profession;
+};
+
+#endif // EMPLOYEE_H
